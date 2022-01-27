51c35233a9f5435c326e47097130cdc9454f48bd
GreatOfTheViktor <84631618+GreatOfTheViktor@users.noreply.github.com>
2022-01-27 21:57:05 +0400

    Create TestApplicationTests.java

---
+++ src/test/java/ru/shop/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shop.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
