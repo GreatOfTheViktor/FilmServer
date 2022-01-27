474b30eb008a4ec2fcf4f1b45a50d4d40abc52ba
GreatOfTheViktor <84631618+GreatOfTheViktor@users.noreply.github.com>
2022-01-27 21:56:28 +0400

    Create Film.java

---
+++ src/main/java/ru/shop/test/models/films/film/Film.java
@@ -0,0 +1,12 @@
+package ru.shop.test.models.films.film;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity(name = "Film")
+@Table(name = "films")
+@Data
+@AllArgsConstructor
+public class Film {
+}
